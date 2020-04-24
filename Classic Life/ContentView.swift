fe66b0fc1028f6e95b2d5078f3e3a38b128e3437
David Reich <dreich@alum.mit.edu>
2020-04-24 16:25:53 +1000

    Initial Commit

---
+++ Classic Life/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Classic Life
+//
+//  Created by David S Reich on 24/4/20.
+//  Copyright © 2020 StellarSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
